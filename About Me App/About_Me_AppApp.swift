cd40913e9b547952e0c90681830132645f7a968f
Alexandra Baer Chan <mingbaer@gmail.com>
2024-04-17 20:55:13 -0500

    Initial Commit

---
+++ About Me App/About_Me_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppApp.swift
+//  About Me App
+//
+//  Created by Alexandra Baer Chan on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
